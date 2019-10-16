9aaff0c3e5f9571b6e6963cfb29d0ccefa812ff2
ManuelRios94 <manuel.ir2302@gmail.com>
2019-10-16 18:57:36 -0300

    State notification fix

--- src/redux/reducers/Shared/Notification/notificationReducer.js
+++ src/redux/reducers/Shared/Notification/notificationReducer.js
@@ -1,22 +1,35 @@
 import initialState from '../../../initialState';
 import { SHOW_ERROR, SHOW_INFO, SHOW_SUCCESS, SHOW_WARNING } from '../../../constants/Shared/Notification/notificationConstants';
-
+import cloneDeep from 'lodash/cloneDeep';
+import merge from 'lodash/merge';
 
 export const notificationReducer = (state = initialState.notification, action) => {
     let new_state
     switch (action.type) {
         case SHOW_ERROR:
-            new_state = {type: SHOW_ERROR, text: action.payload}
-            return new_state;
+            new_state = cloneDeep(state);
+            return merge(new_state, {
+                type: SHOW_ERROR,
+                text: action.payload
+              });
         case SHOW_INFO:
-            new_state = {type: SHOW_INFO, text: action.payload}
-            return new_state;
+            new_state = cloneDeep(state);
+            return merge(new_state, {
+                type: SHOW_INFO,
+                text: action.payload
+              });
         case SHOW_SUCCESS:
-            new_state = {type: SHOW_SUCCESS, text: action.payload}
-            return new_state;
+            new_state = cloneDeep(state);
+            return merge(new_state, {
+                type: SHOW_SUCCESS,
+                text: action.payload
+              });;
         case SHOW_WARNING:
-            new_state = {type: SHOW_WARNING, text: action.payload}
-            return new_state;
+            new_state = cloneDeep(state);
+            return merge(new_state, {
+                type: SHOW_WARNING,
+                text: action.payload
+              });
         default:
             return state;
     }
